cba9593084e635302ee10a70e8e3b6ceda577b64
ig-ealbert <elalbert42@gmail.com>
2022-11-22 18:07:41 -0500

    Updated to modern JavaScript

--- slidingPuzzleGame.js
+++ slidingPuzzleGame.js
@@ -1,116 +1,112 @@
-var gameState = initializeGameState();
-gameState = createSolvablePuzzleState(gameState);
+const messageDiv = document.getElementById("message");
+const resetButton = document.getElementById("restartGame");
+const table = document.getElementById("puzzle");
+
+let gameState = initializeGameState();
+createSolvablePuzzleState(gameState);
 addClickHandlers();
 
 function initializeGameState() {
-	var board = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]; // 0 = x
-	return board;
+  return [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]; // 0 = x
 }
 
-function createSolvablePuzzleState(board) {
-	for (var steps = 0; steps < 100; steps++) {
-		randomMove();
-	}
-	resetMessageAndButton();
-	return board;
+function createSolvablePuzzleState() {
+  for (let steps = 0; steps < 100; steps++) {
+    randomMove();
+  }
+  resetMessageAndButton();
 }
 
 function randomMove() {
-	var validMove = false;
-	var xOffset, yOffset, xSwapLocation, ySwapLocation;
-	var xPiece = findXPiece();
-	while (!validMove) {
-		xOffset = getRandomInt(-1, 1);
-		yOffset = getRandomInt(-1, 1);
-		xSwapLocation = xPiece[0] + xOffset;
-		ySwapLocation = xPiece[1] + yOffset;
-		validMove = isAdjacentToX(xSwapLocation, ySwapLocation) && 
-					isOnBoard(xSwapLocation, ySwapLocation)
-	}
-	swapWithXPiece(xSwapLocation, ySwapLocation);
+  let validMove = false;
+  let xOffset, yOffset, xSwapLocation, ySwapLocation;
+  const xPiece = findXPiece();
+  while (!validMove) {
+    xOffset = getRandomInt(-1, 1);
+    yOffset = getRandomInt(-1, 1);
+    xSwapLocation = xPiece[0] + xOffset;
+    ySwapLocation = xPiece[1] + yOffset;
+    validMove = isAdjacentToX(xSwapLocation, ySwapLocation) && 
+          isOnBoard(xSwapLocation, ySwapLocation)
+  }
+  swapWithXPiece(xSwapLocation, ySwapLocation);
 }
 
 function isOnBoard(x, y) {
-	return x >= 0 && x < 4 && y >= 0 && y < 4;
+  return x >= 0 && x < 4 && y >= 0 && y < 4;
 }
 
 function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function resetMessageAndButton() {
-	var messageDiv = document.getElementById("message");
-	messageDiv.innerHTML = "";
-	var resetButton = document.getElementById("restartGame");
-	resetButton.classList.remove("greenBorder");
+  messageDiv.innerHTML = "";
+  resetButton.classList.remove("greenBorder");
 }
 
 function addClickHandlers() {
-	var table = document.getElementById("puzzle");
-	var cells = table.getElementsByTagName("td");
-	for (var cell of cells) {
-		cell.onclick = function() {
-			swapWithXPiece(this.parentNode.rowIndex, this.cellIndex);
-		};
-	}
+  const cells = table.getElementsByTagName("td");
+  for (const cell of cells) {
+    cell.onclick = function() {
+      swapWithXPiece(this.parentNode.rowIndex, this.cellIndex);
+    };
+  }
 }
 
 function findXPiece() {
-	for (var r = 0; r < gameState.length; r++) {
-		if (gameState[r].indexOf(0) !== -1) {
-			return [r, gameState[r].indexOf(0)];
-		}
-	}
-	return [-1, -1];
+  for (const [r, row] of gameState.entries()) {
+    if (row.indexOf(0) !== -1) {
+      return [r, row.indexOf(0)];
+    }
+  }
+  return [-1, -1];
 }
 
 function swapWithXPiece(clickedRow, clickedCol) {
-	if (isAdjacentToX(clickedRow, clickedCol)) {
-		var xPiece = findXPiece();
-		swapPiecesInUI([clickedRow, clickedCol], xPiece);
-		var temp = gameState[clickedRow][clickedCol];
-		gameState[clickedRow][clickedCol] = 0;
-		gameState[xPiece[0]][xPiece[1]] = temp;
-		updateUIIfGameWon();
-	}
+  if (isAdjacentToX(clickedRow, clickedCol)) {
+    const xPiece = findXPiece();
+    swapPiecesInUI([clickedRow, clickedCol], xPiece);
+    const temp = gameState[clickedRow][clickedCol];
+    gameState[clickedRow][clickedCol] = 0;
+    gameState[xPiece[0]][xPiece[1]] = temp;
+    updateUIIfGameWon();
+  }
 }
 
 function isAdjacentToX(clickedRow, clickedCol) {
-	var xPiece = findXPiece();
-	var above = xPiece[1] === clickedCol && xPiece[0] === clickedRow + 1;
-	var below = xPiece[1] === clickedCol && xPiece[0] === clickedRow - 1;
-	var left = xPiece[1] === clickedCol + 1 && xPiece[0] === clickedRow;
-	var right = xPiece[1] === clickedCol - 1 && xPiece[0] === clickedRow;
-	return above || below || left || right;
+  const xPiece = findXPiece();
+  const above = xPiece[1] === clickedCol && xPiece[0] === clickedRow + 1;
+  const below = xPiece[1] === clickedCol && xPiece[0] === clickedRow - 1;
+  const left = xPiece[1] === clickedCol + 1 && xPiece[0] === clickedRow;
+  const right = xPiece[1] === clickedCol - 1 && xPiece[0] === clickedRow;
+  return above || below || left || right;
 }
 
 function swapPiecesInUI(clickedPiece, xPiece) {
-	var table = document.getElementById("puzzle");
-	var row = table.getElementsByTagName("tr")[clickedPiece[0]];
-	var cell = row.getElementsByTagName("td")[clickedPiece[1]];
-	var xRow = table.getElementsByTagName("tr")[xPiece[0]];
-	var xCell = xRow.getElementsByTagName("td")[xPiece[1]];
-	cell.innerHTML = "x";
-	xCell.innerHTML = gameState[clickedPiece[0]][clickedPiece[1]];
+  const row = table.getElementsByTagName("tr")[clickedPiece[0]];
+  const cell = row.getElementsByTagName("td")[clickedPiece[1]];
+  const xRow = table.getElementsByTagName("tr")[xPiece[0]];
+  const xCell = xRow.getElementsByTagName("td")[xPiece[1]];
+  cell.innerHTML = "x";
+  xCell.innerHTML = gameState[clickedPiece[0]][clickedPiece[1]];
 }
 
 function updateUIIfGameWon() {
-	if (checkForWin()) {
-		var messageDiv = document.getElementById("message");
-		messageDiv.innerHTML = "You won the game!";
-		var resetButton = document.getElementById("restartGame");
-		resetButton.classList.add("greenBorder");
-	}
+  if (checkForWin()) {
+    messageDiv.innerHTML = "You won the game!";
+    resetButton.classList.add("greenBorder");
+  }
 }
 
 function checkForWin() {
-	var winState = initializeGameState();
-	for (var i = 0; i < gameState.length; i++) {
-		for (var j = 0; j < gameState[i].length; j++) {
-			if (gameState[i][j] !== winState[i][j]) {
-				return false;
-			}
-		}
-	}
-	return true;
+  const winState = initializeGameState();
+  for (let i = 0; i < gameState.length; i++) {
+    for (let j = 0; j < gameState[i].length; j++) {
+      if (gameState[i][j] !== winState[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
--- slidingPuzzleTests.js
+++ slidingPuzzleTests.js
@@ -1,51 +1,49 @@
 QUnit.test( "Initially the message div should be blank", function (assert ) {
-  var message = document.getElementById("message");
-  assert.equal( message.innerHTML, "", "No message should be displayed when the game starts." );
+  assert.equal( messageDiv.innerHTML, "", "No message should be displayed when the game starts." );
 });
 
 QUnit.test( "The reset button should not be highlighted when the game starts", function (assert ) {
-  var resetButton = document.getElementById("restartGame");
   assert.equal( resetButton.classList.contains("greenBorder"), false, "The reset button should not have a green border." );
 });
 
 QUnit.test( "The numbers should be ordered before scrambling them", function (assert ) {
   gameState = initializeGameState();
-  var expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
+  const expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
   assert.deepEqual( gameState, expected, "The numbers should be ordered." );
 });
 
 QUnit.test( "The numbers should not be ordered after scrambling them", function (assert ) {
   gameState = initializeGameState();
   gameState = createSolvablePuzzleState(gameState);
-  var expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
+  const expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
   assert.notDeepEqual( gameState, expected, "The numbers should not be ordered." );
 });
 
 QUnit.test( "Clicking on the number to the left of the x should swap the two pieces", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapWithXPiece(2, 1);
-  var expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 0, 10, 11], [13, 14, 15, 12]];
+  const expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 0, 10, 11], [13, 14, 15, 12]];
   assert.deepEqual( gameState, expected, "The pieces should be swapped." );
 });
 
 QUnit.test( "Clicking on the number to the right of the x should swap the two pieces", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapWithXPiece(2, 3);
-  var expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0], [13, 14, 15, 12]];
+  const expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0], [13, 14, 15, 12]];
   assert.deepEqual( gameState, expected, "The pieces should be swapped." );
 });
 
 QUnit.test( "Clicking on the number above the x should swap the two pieces", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapWithXPiece(1, 2);
-  var expected = [[1, 2, 3, 4], [5, 6, 0, 8], [9, 10, 7, 11], [13, 14, 15, 12]];
+  const expected = [[1, 2, 3, 4], [5, 6, 0, 8], [9, 10, 7, 11], [13, 14, 15, 12]];
   assert.deepEqual( gameState, expected, "The pieces should be swapped." );
 });
 
 QUnit.test( "Clicking on the number below the x should swap the two pieces", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapWithXPiece(3, 2);
-  var expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 15, 11], [13, 14, 0, 12]];
+  const expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 15, 11], [13, 14, 0, 12]];
   assert.deepEqual( gameState, expected, "The pieces should be swapped." );
 });
 
@@ -62,10 +60,9 @@ QUnit.test( "If the X piece does not exist, it should not be found", function( a
 QUnit.test( "All spaces should have an event handler", function (assert ) {
   gameState = initializeGameState();
   addClickHandlers();
-  var areThereClickHandlers = true;
-  var table = document.getElementById("puzzle");
-  var cells = table.getElementsByTagName("td");
-  for (var cell of cells) {
+  let areThereClickHandlers = true;
+  const cells = table.getElementsByTagName("td");
+  for (const cell of cells) {
     areThereClickHandlers = areThereClickHandlers && (typeof cell.onclick == "function");
   }
   assert.deepEqual(areThereClickHandlers, true, "All spaces have event handlers." );
@@ -94,90 +91,80 @@ QUnit.test( "The piece below the x is adjacent to it", function( assert ) {
 QUnit.test( "The piece to the left of the x should become the x when clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([2, 1], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var row = table.getElementsByTagName("tr")[2];
-  var cell = row.getElementsByTagName("td")[1];
+  const row = table.getElementsByTagName("tr")[2];
+  const cell = row.getElementsByTagName("td")[1];
   assert.equal( cell.innerHTML, "x", "The pieces should be swapped." );
 });
 
 QUnit.test( "The x piece should become the piece to the left when the piece to the left is clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([2, 1], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var xRow = table.getElementsByTagName("tr")[2];
-  var xCell = xRow.getElementsByTagName("td")[2];
+  const xRow = table.getElementsByTagName("tr")[2];
+  const xCell = xRow.getElementsByTagName("td")[2];
   assert.equal( xCell.innerHTML, "10", "The pieces should be swapped." );
 });
 
 QUnit.test( "The piece to the right of the x should become the x when clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([2, 3], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var row = table.getElementsByTagName("tr")[2];
-  var cell = row.getElementsByTagName("td")[3];
+  const row = table.getElementsByTagName("tr")[2];
+  const cell = row.getElementsByTagName("td")[3];
   assert.equal( cell.innerHTML, "x", "The pieces should be swapped." );
 });
 
 QUnit.test( "The x piece should become the piece to the right when the piece to the right is clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([2, 3], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var xRow = table.getElementsByTagName("tr")[2];
-  var xCell = xRow.getElementsByTagName("td")[2];
+  const xRow = table.getElementsByTagName("tr")[2];
+  const xCell = xRow.getElementsByTagName("td")[2];
   assert.equal( xCell.innerHTML, "11", "The pieces should be swapped." );
 });
 
 QUnit.test( "The piece above the x should become the x when clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([1, 2], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var row = table.getElementsByTagName("tr")[1];
-  var cell = row.getElementsByTagName("td")[2];
+  const row = table.getElementsByTagName("tr")[1];
+  const cell = row.getElementsByTagName("td")[2];
   assert.equal( cell.innerHTML, "x", "The pieces should be swapped." );
 });
 
 QUnit.test( "The x piece should become the piece above when the piece above is clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([1, 2], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var xRow = table.getElementsByTagName("tr")[2];
-  var xCell = xRow.getElementsByTagName("td")[2];
+  const xRow = table.getElementsByTagName("tr")[2];
+  const xCell = xRow.getElementsByTagName("td")[2];
   assert.equal( xCell.innerHTML, "7", "The pieces should be swapped." );
 });
 
 QUnit.test( "The piece below the x should become the x when clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([3, 2], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var row = table.getElementsByTagName("tr")[3];
-  var cell = row.getElementsByTagName("td")[2];
+  const row = table.getElementsByTagName("tr")[3];
+  const cell = row.getElementsByTagName("td")[2];
   assert.equal( cell.innerHTML, "x", "The pieces should be swapped." );
 });
 
 QUnit.test( "The x piece should become the piece below when the piece below is clicked", function( assert ) {
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 0, 11], [13, 14, 15, 12]];
   swapPiecesInUI([3, 2], [2, 2]);
-  var table = document.getElementById("puzzle");
-  var xRow = table.getElementsByTagName("tr")[2];
-  var xCell = xRow.getElementsByTagName("td")[2];
+  const xRow = table.getElementsByTagName("tr")[2];
+  const xCell = xRow.getElementsByTagName("td")[2];
   assert.equal( xCell.innerHTML, "15", "The pieces should be swapped." );
 });
 
 QUnit.test( "The game is won when the numbers are in order", function (assert ) {
   gameState = initializeGameState();
-  var expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
+  expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
   assert.equal( checkForWin(), true, "The game has been won." );
 });
 
 QUnit.test( "A message is displayed if the game has been won", function (assert ) {
-  var message = document.getElementById("message");
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
   swapWithXPiece(3, 3);
-  assert.equal( message.innerHTML, "You won the game!", "A message is displayed to show that the player won." );
+  assert.equal( messageDiv.innerHTML, "You won the game!", "A message is displayed to show that the player won." );
 });
 
 QUnit.test( "The reset button should be highlighted when the game is won", function (assert ) {
-  var resetButton = document.getElementById("restartGame");
   gameState = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
   swapWithXPiece(3, 3);
   assert.equal( resetButton.classList.contains("greenBorder"), true, "The reset button should have a green border." );
